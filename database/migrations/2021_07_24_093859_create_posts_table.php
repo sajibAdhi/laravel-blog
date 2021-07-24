8d2bd9627d4c0227eacd0cd1e1fbe298332a8f3b
sajibAdhi <sajibadhikery@gmail.com>
2021-07-24 15:45:34 +0600

    create_posts_table migration

--- database/migrations/2021_07_24_093859_create_posts_table.php
+++ database/migrations/2021_07_24_093859_create_posts_table.php
@@ -15,7 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('excerpt');
+            $table->string('body');
             $table->timestamps();
+            $table->timestamp('published_at')->nullable();
         });
     }
 
