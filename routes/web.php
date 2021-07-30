6e403f86b9b4e58ac6a216d2ee332349c724dd4b
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-07-30 23:49:41 +0600

    Eager Load Relationships on an Existing Model

--- routes/web.php
+++ routes/web.php
@@ -36,12 +36,12 @@ Route::get('/posts/{post:slug}', function (Post $post) { // Post::where('slug', 
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts->load(['category', 'author']),
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts->load(['category', 'author']),
     ]);
 });
