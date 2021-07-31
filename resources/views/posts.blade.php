450fa43a776ea3bc69cac43b34c4be3fbd234823
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-07-31 22:09:41 +0600

    Blade Components and CSS Grids

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,39 +3,11 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        @if ($posts->count())
+            <x-post-grid :posts="$posts" />
+        @else
+            <p class="text-center">No Post Yet. please Check Back Later</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
     </main>
-    {{-- @foreach ($posts as $post)
-        <article>
-            <h1>
-
-                <a href="{{ url("/posts/$post->slug") }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-            <p>
-                By <a href="{{ url('/authors/' . $post->author->username) }}">{{ $post->author->name }}</a> in
-                <a href="{{ url('categories/' . $post->category->slug) }}">
-                    {{ $post->category->name }}
-                </a>
-            </p>
-            <div>
-                <p>
-                    {{ $post->excerpt }}
-                </p>
-            </div>
-        </article>
-    @endforeach --}}
 </x-layout>
