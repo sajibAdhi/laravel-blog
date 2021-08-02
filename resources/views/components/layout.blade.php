13c4732cf441bd7ca84a9c0c98170e85336cf7f0
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-08-02 13:03:03 +0600

    TODO: A Small JavaScript Dropdown Detour

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,9 +4,16 @@
 <head>
 
     <title>Laravel From Scratch Blog</title>
+
+    {{-- TailWind CSS --}}
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    {{-- Google Fonts --}}
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+    {{-- Alpine JS --}}
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
 </head>
 
