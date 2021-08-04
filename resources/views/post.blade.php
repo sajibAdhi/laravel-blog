c5c62a3054fb27df4e46f4c5cab282b22d3448ce
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-08-04 16:28:52 +0600

    Quick Tweaks and Clean-Up

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -44,7 +44,7 @@
 
                 <div class="space-y-4 lg:text-lg leading-loose">
                     <p>
-                        {{ $post->body }}
+                        {!! $post->body !!}
                     </p>
                 </div>
             </div>
