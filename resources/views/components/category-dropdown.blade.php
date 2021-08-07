4fe5233c2e16a438e279e87360bbcb703970baf3
sajibAdhi <sajibadhikery@gmail.com>
2021-08-07 17:22:24 +0600

    Laughably Simple Pagination

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,13 +7,16 @@
         </button>
     </x-slot>
 
-    <x-dropdown-item href="/" :active="request()->routeIs('home')">
+    <x-dropdown-item
+        href="/?{{http_build_query(request()->except('category', 'page'))}}"
+        :active="request()->routeIs('home')"
+    >
         All
     </x-dropdown-item>
     @foreach ($categories as $category)
 
         <x-dropdown-item
-            href="/posts?category={{ $category->slug }}&{{http_build_query(request()->except('category'))}}"
+            href="/posts?category={{ $category->slug }}&{{http_build_query(request()->except('category', 'page'))}}"
             :active="request()->is('categories/'.$category->slug)"
         >
             {{ ucwords($category->name) }}
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,8 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
             <x-posts-grid :posts="$posts" />
+
+            {{ $posts->links() }}
         @else
             <p class="text-center">No Post Yet. please Check Back Later</p>
         @endif
