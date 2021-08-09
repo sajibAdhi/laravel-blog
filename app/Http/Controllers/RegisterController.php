80d2c95fdf7816c083312a28d60dead53cd9ddfb
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-08-09 21:50:24 +0600

    Failed Validation and Old Input Data

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -40,9 +40,9 @@ class RegisterController extends Controller
     public function store()
     {
         $attributes = request()->validate([
-            'name' => 'required|max:255|min:3',
-            'username' => 'required|max:255|min:3',
-            'email' => 'required|email|max:255',
+            'name' => 'required|min:3|max:255',
+            'username' => 'required|min:3|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|min:7 ',
         ]);
 
