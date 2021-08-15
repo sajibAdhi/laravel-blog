598255ecb147ea406f662bb6419e656e93e9b5b7
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-08-15 16:17:09 +0600

    Write the Markup for a Post Comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,22 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-8">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" height="60" width="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h1 class="font-bold"> John Doe</h1>
+            <p class="text-xs">
+                Posted <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>
+            asdasddddddddddddddddddddddddddddddd
+            asddddddddddddddddddddddddddddddddd
+            sssssssssssssssssssssssssssssssss
+            eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+            gfffffffffffffffffffffffffffffff
+        </p>
+    </div>
+</article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,10 +10,10 @@
                 </p>
 
                 <div class="flex items-center lg:justify-center text-sm mt-4">
-                    <img src="{{url('images/lary-avatar.svg')}}" alt="Lary avatar">
+                    <img src="{{ url('images/lary-avatar.svg') }}" alt="Lary avatar">
                     <div class="ml-3 text-left">
                         <h5 class="font-bold">
-                            <a href="{{ url('posts?authors='. $post->author->username) }}">
+                            <a href="{{ url('posts?authors=' . $post->author->username) }}">
                                 {{ $post->author->name }}
                             </a>
                         </h5>
@@ -24,13 +24,13 @@
             <div class="col-span-8">
                 <div class="hidden lg:flex justify-between mb-6">
                     <a href="/"
-                       class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
+                        class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
                         <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
                             <g fill="none" fill-rule="evenodd">
                                 <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
                                 </path>
                                 <path class="fill-current"
-                                      d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
                                 </path>
                             </g>
                         </svg>
@@ -39,7 +39,7 @@
                     </a>
 
                     <div class="space-x-2">
-                        <x-category-button :category="$post->Category"/>
+                        <x-category-button :category="$post->Category" />
                     </div>
                 </div>
 
@@ -53,6 +53,12 @@
                     </p>
                 </div>
             </div>
+            <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                <x-post-comment />
+                <x-post-comment />
+                <x-post-comment />
+                <x-post-comment />
+            </section>
         </article>
     </main>
 </x-layout>
